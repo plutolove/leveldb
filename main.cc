55412e275b830c9a6c1e924f64fcc3dd08a9b787
mengshangqi <mengshangqis@gmail.com>
2017-10-21 12:27:31 +0800

    test

--- main.cc
+++ main.cc
@@ -3,10 +3,28 @@
 //
 
 #include <iostream>
+#include <cassert>
+#include "leveldb/db.h"
 typedef long long LL;
+using namespace std;
 int main() {
-    LL a00, a01, a10, a11;
-    std::cin >> a00 >> a01 >> a10 >> a11;
+  leveldb::DB *db;
+  leveldb::Options options;
+  options.create_if_missing = true;
+  leveldb::Status status = leveldb::DB::Open(options, "/home/meng/CLionProjects/db/", &db);
+  assert(status.ok());
+  string key = "name";
+  string value = "mengshangqi";
+  status = db->Put(leveldb::WriteOptions(), key, value);
+  assert(status.ok());
 
-    return 0;
+  // read
+  status = db->Get(leveldb::ReadOptions(), key, &value);
+  assert(status.ok());
+  cout << value << endl;
+  for(int i=0; i<50000; i++) {
+    status = db->Put(leveldb::WriteOptions(), to_string(i)+"dfgdf", to_string(i)+"fdhfghjdfg");
+    assert(status.ok());
+  }
+  return 0;
 }
